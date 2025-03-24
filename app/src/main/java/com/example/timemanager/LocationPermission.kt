f2dbab31eb966c70d72dad7cc3ab0abb9d71240c
tmillner21 <tmillner@ufl.edu>
2025-03-24 18:12:29 -0400

    some pages are working

---
+++ app/src/main/java/com/example/timemanager/LocationPermission.kt
@@ -0,0 +1,31 @@
+package com.example.timemanager
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LocationPermission : AppCompatActivity() {
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.location_permission)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button = findViewById(R.id.yes_location_button)
+        button.setOnClickListener {
+            val intent = Intent(this@LocationPermission, TaskList::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/timemanager/TripDecision.kt
@@ -0,0 +1,31 @@
+package com.example.timemanager
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TripDecision : AppCompatActivity() {
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.trip_decision)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button = findViewById(R.id.new_trip_button)
+        button.setOnClickListener {
+            val intent = Intent(this@TripDecision, LocationPermission::class.java)
+            startActivity(intent)
+        }
+    }
+}
