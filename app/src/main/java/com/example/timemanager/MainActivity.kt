df120f8ff175ed9b64ef4af8ae50f012fb7a9ec5
tmillner21 <tmillner@ufl.edu>
2025-03-26 16:35:52 -0400

    Added comments so yall can follow what the code does

--- app/src/main/java/com/example/timemanager/MainActivity.kt
+++ app/src/main/java/com/example/timemanager/MainActivity.kt
@@ -10,20 +10,24 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    // for button
     private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // this stuff is about the display
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // Change the R.layout to whatever your xml file is
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        button = findViewById(R.id.button)
-        button.setOnClickListener {
+        // actual button functionality
+        button = findViewById(R.id.button) // change R.id to the button you want to get clicked on
+        button.setOnClickListener { // change this@MainActivity to the current class youre in
+            // and change TripDecision::class.java to the class you want to transition to
             val intent = Intent(this@MainActivity, TripDecision::class.java)
             startActivity(intent)
         }
